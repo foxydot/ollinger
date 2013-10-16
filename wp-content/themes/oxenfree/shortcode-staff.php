19b111a268aba76ef1688ae14805bd33cb43b072
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-10-16 15:43:06 -0400

    styling staff and adjusting more font stuff

---
+++ wp-content/themes/oxenfree/shortcode-staff.php
@@ -0,0 +1,30 @@
+<?php $t =& peTheme(); ?>
+<?php $content =& $t->content; ?>
+<?php $meta =& $t->content->meta(); ?>
+<div class="team-member">
+    <div class="member-info">
+    	<div class="member-photo">
+    		<div class="bw-wrapper">
+    			<img src="<?php echo $t->image->resizedImgUrl($content->get_origImage(),220,220); ?>"/>
+    		</div>
+    	</div>
+    	<div class="member-social">
+    		<h4><?php $content->title(); ?></h4>
+    		<span class="position"><?php echo $meta->info->position; ?></span>
+    		<ul class="member-social-links">
+    			<?php if ($link = $meta->info->twitter): ?>
+    			<li><a href="<?php echo $link ?>">Twitter</a></li>
+    			<?php endif; ?>
+    			<?php if ($link = $meta->info->linkedin): ?>
+    			<li><a href="<?php echo $link ?>">LinkedIn</a></li>
+    			<?php endif; ?>
+    			<?php if ($link = $meta->info->email): ?>
+    			<li><a href="mailto:<?php echo $link ?>">email</a></li>
+    			<?php endif; ?>
+    		</ul>
+		</div>
+	</div>
+	<div class="member-bio">
+	    <?php $content->content(); ?>
+	</div>
+</div>
